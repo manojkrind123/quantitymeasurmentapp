56c55cfb12f813878769618b00f44acd64113eba
MA20344154 <manoj.kumar319@wipro.com>
2026-01-28 17:35:13 +0530

    QuantityMeasurmentApp UC1 with test fixed junit issue

--- src/com/test/QuantityMeasurmentAppTest.java
+++ src/com/test/QuantityMeasurmentAppTest.java
@@ -8,8 +8,8 @@ public class QuantityMeasurmentAppTest {
    @Test
     public void testFeetEquality_SameValue(){
 
-       Feet f1 = new Feet(1.0);
-       Feet f2 = new Feet(1.0);
+       Feet f1 = new Feet(2.0);
+       Feet f2 = new Feet(2.0);
        assert(f1.equals(f2));
 
 
