40f912b514ad042208352d30d99ae29cd655792f
MA20344154 <manoj.kumar319@wipro.com>
2026-01-29 12:23:19 +0530

    QuantityMeasurmentApp UC1 with test cases done

--- src/com/measurment/QuantityMeasurmentApp.java
+++ src/com/measurment/QuantityMeasurmentApp.java
@@ -4,9 +4,18 @@ public class QuantityMeasurmentApp {
 
     public static class Feet {
 
-        private final double value;
+        private final Double value;
 
-        public Feet(double value) {
+        public Feet(Double value) {
+            if(value == null){
+
+                throw new IllegalArgumentException("value can not be null");
+            }
+            if (Double.isNaN(value) || Double.isInfinite(value)) {
+
+                throw new IllegalArgumentException("invalid numberic value");
+            }
+
             this.value = value;
         }
 
@@ -26,37 +35,36 @@ public class QuantityMeasurmentApp {
         }
     }
 
-        public boolean  compareFeet(double d1, double d2){
+    public boolean compareFeet(double d1, double d2) {
 
-            if(Double.isNaN(d1) || Double.isNaN(d2)){
+        if (Double.isNaN(d1) || Double.isNaN(d2)) {
 
-                throw new IllegalArgumentException("invalid numbers : NaN not allowed");
-            }
-
-            if(Double.isInfinite(d1) || Double.isInfinite(d2)){
-
-                throw new IllegalArgumentException("invalid numbers : infinite values not allowed");
-            }
-            Feet f1 = new Feet(d1);
-            Feet f2 = new Feet(d2);
-
-            return f1.equals(f2);
+            throw new IllegalArgumentException("invalid numbers : NaN not allowed");
         }
+
+        if (Double.isInfinite(d1) || Double.isInfinite(d2)) {
+
+            throw new IllegalArgumentException("invalid numbers : infinite values not allowed");
+        }
+        Feet f1 = new Feet(d1);
+        Feet f2 = new Feet(d2);
+
+        return f1.equals(f2);
+    }
 
 
     public static void main(String[] args) {
 
 
-
-       double feet1 =12.0;
-        double feet2 =12.0;
+        double feet1 = 12.0;
+        double feet2 = 12.0;
 
 
         QuantityMeasurmentApp measurmentApp = new QuantityMeasurmentApp();
 
-       boolean result= measurmentApp.compareFeet(feet1,feet2);
+        boolean result = measurmentApp.compareFeet(feet1, feet2);
 
-      System.out.println("Are both values equal   ==  "+ result );
+        System.out.println("Are both values equal   ==  " + result);
 
 
     }
--- src/com/test/QuantityMeasurmentAppTest.java
+++ src/com/test/QuantityMeasurmentAppTest.java
@@ -1,18 +1,54 @@
 package com.test;
 
 import com.measurment.QuantityMeasurmentApp.Feet;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 public class QuantityMeasurmentAppTest {
 
-   @Test
-    public void testFeetEquality_SameValue(){
+    @Test
+    public void testFeetEquality_SameValue() {
 
-       Feet f1 = new Feet(2.0);
-       Feet f2 = new Feet(2.0);
-       assert(f1.equals(f2));
+        Feet f1 = new Feet(2.0);
+        Feet f2 = new Feet(2.0);
+        Assertions.assertTrue(f1.equals(f2));
+
+    }
+
+    @Test
+    public void testFeetEquality_DiffrenceValue() {
+
+        Feet f1 = new Feet(2.0);
+        Feet f2 = new Feet(1.0);
+        Assertions.assertFalse(f1.equals(f2));
+
+    }
+
+    @Test
+    public void testFeetEquality_NullComprasion() {
+
+        Feet f1 = new Feet(2.0);
+        Assertions.assertFalse(f1.equals(null));
+
+    }
+
+    @Test
+    public void testFeetEquality_SameRefrence() {
+
+        Feet f1 = new Feet(2.0);
+        Assertions.assertTrue(f1.equals(f1));
+
+    }
+
+    @Test
+    public void testFeetEquality_NonNumericInput() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{new Feet(Double.NaN);});
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{new Feet(Double.POSITIVE_INFINITY);});
 
 
+    }
 
-   }
+
 }
